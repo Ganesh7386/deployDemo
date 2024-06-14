7302f1f6b7b888df203c6eceeb29a2944dd191f6
Ganesh7386 <puttaganesh7386@gmail.com>
2024-06-14 19:48:34 +0530

    added scraping logic commit

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
+const {startScraping} = require("./scraper")
 const express = require("express");
+// const serverless = require("serverless-http");
+const cors = require("cors");
+var bodyParser = require('body-parser')
 
-const app = express();
+app = express()
+const port = process.env.port || 5000
+// const router = express.Router();
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.json())
 
-const port = process.env.port || 5000;
+let listOfArticles = []
 
-app.use(express.json());
+// app.get("/:id/" , (req , res)=> {
+//     const {id} = req.params;
+//     console.log(id)
 
-app.get("/" , (req , res)=> {
-    res.send("Hello from server");
+//     res.status(200).json({givenId : id})
+// })
+
+app.post("/scrape/" , async (req , res)=> {
+    console.log(req.body);
+    const {prompt} = req.body;
+    console.log(prompt)
+    try {
+        const resultsList = await startScraping(prompt);
+        if(resultsList.ok) {
+            console.log("retrieved successfully");
+        console.log(resultsList.scrapedDataList)
+        console.log("in server");
+        res.status(200).json({ok : true , searchResults : resultsList.scrapedDataList})
+        }
+        else {
+            res.status(400).json({ok : false , errorMsg : "server side , document not loaded , Please try again"})
+        }
+    }
+    catch(e) {
+        console.log("document not loaded");
+        res.status(400).json({ok : false , errorMsg : "server side , document not loaded , Please try again"});
+    }
 })
 
-app.post("/register/" , (req , res)=> {
-    const {email} = req.body;
-    res.status(200).send({sentEmail : email})
-})
+
 
 
 app.listen(port , ()=> {
-    console.log(`Server started at http://localhost:${port}`);
+    console.log(`server started at http://localhost:${port}`);
 })
-
